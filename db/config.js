dfdeeb5751236fc176f875e1ad20738a90ac532e
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-09-10 15:25:25 -0700

    Created index.js file and a database(db) folder for sql files.

---
+++ db/config.js
@@ -0,0 +1,18 @@
+// Import required module
+const mysql = require("mysql2");
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root", // Database password
+  database: "business_db", // Database name
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) throw err;
+});
+
+// Export the connection for use in other modules
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import the application menu module
+const prompt = require("./utils/prompt");
+
+// Start the application menu
+function startApp() {
+  // Start the application menu
+  prompt.menu();
+}
+
+// Call the function to start the application
+startApp();
