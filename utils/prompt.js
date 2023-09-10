591a362b7aad801d2c5e3466304d5d3cbf9a1ec5
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-09-10 15:58:28 -0700

    Created utils folder for all my js functions. The prompt.js currently has the inquirer.prompt and exit program function.

---
+++ utils/prompt.js
@@ -0,0 +1,96 @@
+// Import required modules and functions
+const inquirer = require("inquirer");
+const connection = require("../db/config"); // Database configuration
+require("console.table"); // Import console.table for displaying data in tabular format
+
+// Display the main menu, handle user choices, and manage program flow
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "Select an option:",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee's role",
+          "Update an employee's manager",
+          "View employees by manager",
+          "View employees by department",
+          "View department budget",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+          "Exit Program",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.menu) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee's role":
+          updateEmployeeRole();
+          break;
+        case "Update an employee's manager":
+          updateEmployeeManager();
+          break;
+        case "View employees by manager":
+          viewEmployeesByManager();
+          break;
+        case "View employees by department":
+          viewEmployeesByDepartment();
+          break;
+        case "View department budget":
+          viewDepartmentBudget();
+          break;
+        case "Delete a department":
+          deleteDepartment();
+          break;
+        case "Delete a role":
+          deleteRole();
+          break;
+        case "Delete an employee":
+          deleteEmployee();
+          break;
+        case "Exit Program":
+          exitProgram();
+          break;
+      }
+    });
+}
+
+// Function to handle program exit
+function exitProgram() {
+  console.log("Thank you for using Employee Tracker. Goodbye! 👋");
+  connection.end((err) => {
+    if (err) {
+      console.error("Error closing the database connection:", err);
+    } else {
+      console.log("Database connection closed.");
+    }
+  });
+}
+
+module.exports = { menu };
